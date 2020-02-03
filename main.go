cdb2f8d6c9ad6a34c7dbdf5ca39bee47be3af482
wahyu <wahyu.kukuh.herlambang@gmail.com>
2020-02-03 13:55:00 +0700

    implement sudoku checker

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+)
+
+var N = 9
+
+func main() {
+	cases := []Cases{
+		Case1, Case2,
+	}
+
+	for _, c := range cases {
+		puzzle := Puzzle{
+			Board: c,
+		}
+
+		fmt.Println(puzzle.IsValidSudoku())
+	}
+}
+
+type Puzzle struct {
+	Board [][]int
+}
+
+func (p Puzzle) IsValidSudoku() bool {
+	for row := 0; row < N; row++ {
+		for col := 0; col < N; col++ {
+			if !p.isDigitValid(row, col, p.Board[row][col]) {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (p Puzzle) isDigitValid(row, col, digit int) bool {
+	// note: integer division 'rounds down' by ignoring all decimal places
+	startRow := row / 3 * 3
+	startCol := col / 3 * 3
+
+	var isFirstDataH bool
+	var isFirstDataV bool
+	var isFirstData3x3 bool
+
+	for i := 0; i < N; i++ {
+		// check the corresponding row and column
+		if (p.Board[row][i] == digit ||
+			p.Board[i][col] == digit ||
+			// check the corresponding 3x3 section
+			p.Board[startRow+i/3][startCol+i%3] == digit) && (isFirstDataH && isFirstDataV && isFirstData3x3) {
+			return false
+		}
+
+		if p.Board[row][i] == digit && !isFirstDataH {
+			isFirstDataH = true
+		}
+
+		if p.Board[i][col] == digit && !isFirstDataV {
+			isFirstDataV = true
+		}
+
+		if p.Board[startRow+i/3][startCol+i%3] == digit && !isFirstData3x3 {
+			isFirstData3x3 = true
+		}
+	}
+
+	return true
+}
