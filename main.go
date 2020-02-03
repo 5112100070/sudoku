6ef640a8ef329e4cbbd4e13e9e0eb3e1d4e4d975
wahyu <wahyu.kukuh.herlambang@gmail.com>
2020-02-03 14:31:21 +0700

    add some test case and saparate struct puzzle

--- main.go
+++ main.go
@@ -8,64 +8,21 @@ var N = 9
 
 func main() {
 	cases := []Cases{
-		Case1, Case2,
+		Case1, Case2, Case3, Case4, Case5, Case6, Case7,
 	}
 
-	for _, c := range cases {
+	for i, c := range cases {
 		puzzle := Puzzle{
 			Board: c,
 		}
 
-		fmt.Println(puzzle.IsValidSudoku())
+		var isValidStr string
+		if puzzle.IsValidSudoku() {
+			isValidStr = "Valid"
+		} else {
+			isValidStr = "Not Valid"
+		}
+
+		fmt.Println(fmt.Sprintf("SUDOKU Case Number: %d is %s", i+1, isValidStr))
 	}
-}
-
-type Puzzle struct {
-	Board [][]int
-}
-
-func (p Puzzle) IsValidSudoku() bool {
-	for row := 0; row < N; row++ {
-		for col := 0; col < N; col++ {
-			if !p.isDigitValid(row, col, p.Board[row][col]) {
-				return false
-			}
-		}
-	}
-
-	return true
-}
-
-func (p Puzzle) isDigitValid(row, col, digit int) bool {
-	// note: integer division 'rounds down' by ignoring all decimal places
-	startRow := row / 3 * 3
-	startCol := col / 3 * 3
-
-	var isFirstDataH bool
-	var isFirstDataV bool
-	var isFirstData3x3 bool
-
-	for i := 0; i < N; i++ {
-		// check the corresponding row and column
-		if (p.Board[row][i] == digit ||
-			p.Board[i][col] == digit ||
-			// check the corresponding 3x3 section
-			p.Board[startRow+i/3][startCol+i%3] == digit) && (isFirstDataH && isFirstDataV && isFirstData3x3) {
-			return false
-		}
-
-		if p.Board[row][i] == digit && !isFirstDataH {
-			isFirstDataH = true
-		}
-
-		if p.Board[i][col] == digit && !isFirstDataV {
-			isFirstDataV = true
-		}
-
-		if p.Board[startRow+i/3][startCol+i%3] == digit && !isFirstData3x3 {
-			isFirstData3x3 = true
-		}
-	}
-
-	return true
 }
